8906f8485462572647b18abd6499af9ea531963c
minsukim <minsu980107@naver.com>
2023-07-22 10:47:50 +0900

    feat(backend): HTTPS
    Co-authored-by: minsukim <minsu980107@naver.com>
    Co-authored-by: jhmin95 <tesla52@naver.com>
    Co-authored-by: yeonseong <yeonseong.dev@gmail.com>
    Co-authored-by: minsuki2 <minsukim@Minsus-Mac-mini.local>
    Co-authored-by: juha <contemplation.person@gmail.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./cert.key'),
+    cert: fs.readFileSync('./cert.crt'),
+  };
+
+  const server = express();
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   //  TODO : port should be in config
   app.enableCors(
     {
@@ -12,9 +20,13 @@ async function bootstrap() {
       'preflightContinue': false,
       'optionsSuccessStatus': 204
     }
-  )
+  );
   app.use(cookieParser());
+  app.use(server);
+
   await app.listen(4242);
-  console.log('🚀 Server ready at: http://localhost:4242');
+
+  console.log('🚀 Server ready at: https://localhost:4242');
 }
+
 bootstrap();
