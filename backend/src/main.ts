3b385bcb126896269542d836bc724c3c33b24ea3
yeonseong <yeonseong.dev@gmail.com>
2023-07-22 16:13:19 +0900

    fix(frontend): set cookie success with AXIOS!!!!!!!!!!!

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
       'origin': '*',
       'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
       'preflightContinue': false,
-      'optionsSuccessStatus': 204
+      'optionsSuccessStatus': 204,
+      'credentials': true,
     }
   );
   app.use(cookieParser());
--- frontend/src/pages/login/callback.tsx
+++ frontend/src/pages/login/callback.tsx
@@ -5,8 +5,7 @@ export default function LoginCallback() {
   const router = useRouter();
   if (router.isReady === false) return (<div>Loading...</div>);
   const code = router.query.code;
-  console.log(`${process.env.NEXT_PUBLIC_API_URL}/login/token?code=${code}`);
-  const result = axios.get(`${process.env.NEXT_PUBLIC_API_URL}/login/token?code=${code}`);
+  const result = axios.get(`${process.env.NEXT_PUBLIC_API_URL}/login/token?code=${code}`, {withCredentials: true});
   console.log(result);
   return (
     <div>
