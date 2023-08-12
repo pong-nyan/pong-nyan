47348fe8d4b5ade54b38338ae32d185f7a2ae8f4
minsuki2 <minsukim@Minsus-Mac-mini.local>
2023-08-12 16:46:07 +0900

    feat(docker): database docker setting finish

---
+++ backend/src/database/datasource.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entity/User';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: true,
+      logging: true,
+      entities: [User],
+    }),
+  ],
+})
+export class DatabaseModule {}
