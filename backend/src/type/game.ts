eafa5cef32c2bf65e0792518c93c2373f65ae242
yeonseong <yeonseong.dev@gmail.com>
2023-08-28 22:57:05 +0900

    feat(game): add type for game

---
+++ backend/src/type/game.ts
@@ -0,0 +1,12 @@
+export type BallInfo = {
+    position: {
+        x: number;
+        y: number;
+    };
+    velocity: {
+        x: number;
+        y: number;
+    };
+}
+
+export type RoomName = string;
