ac1f5fccf1a317713580652e81a7319a99f1f1ef
yeonseong <yeonseong.dev@gmail.com>
2023-08-31 21:08:10 +0900

    25 custom button (#26)
    * style(frontend): delete unused code
    
    * feat(frontend): add custon button example
    
    * refactor(image): change cammelcase
    
    * chore(image): :art: images
    
    * Update frontend/src/components/button/CatButton.tsx
    
    Co-authored-by: Ji-Hyuck, Min <45284810+jhMin95@users.noreply.github.com>
    
    * Update frontend/src/pages/_document.tsx
    
    Co-authored-by: Ji-Hyuck, Min <45284810+jhMin95@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: conteng <contemplation.person@gmail.com>
    Co-authored-by: Ji-Hyuck, Min <45284810+jhMin95@users.noreply.github.com>

---
+++ frontend/src/components/button/CatButton.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+type CatButtonProps = {
+    onClickFunction: () => void;
+    width: number;
+    height: number;
+};
+
+const CatButton = ({ onClickFunction, width, height }: CatButtonProps) => {
+  return (
+    <button>
+      <Image src="/assets/cat-footprint.png" alt="catFootprint" onClick={onClickFunction} width={width ?? 42} height={height ?? 42} />
+    </button>
+  );
+};
+
+CatButton.defaultProps = {
+  onClickFunction: () => { console.error('onClickFunction is not defined');},
+  width: 42,
+  height: 42,
+};
+
+export default CatButton;
