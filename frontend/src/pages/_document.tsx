0a264348916eb2e7e47f8d451208b322885127c0
yeonseong <yeonseong.dev@gmail.com>
2023-08-31 14:58:44 +0900

    Update frontend/src/pages/_document.tsx
    Co-authored-by: Ji-Hyuck, Min <45284810+jhMin95@users.noreply.github.com>

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en"> 
       <Head> 
-        <link rel="icon" href="pongNyan.png"/> 
+        <link rel="icon" href="pong-nyan.png"/> 
       </Head>
       <body>
         <Main />
