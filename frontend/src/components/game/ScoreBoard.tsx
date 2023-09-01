448d2ee2f70a16abfafabde1c0bca2f9665b05b7
minsuki2 <minsuki2@student.42seoul.kr>
2023-09-01 22:19:43 +0900

    chores: 주석 정리

--- frontend/src/components/game/ScoreBoard.tsx
+++ frontend/src/components/game/ScoreBoard.tsx
@@ -1,39 +1,5 @@
-import{ useState, useEffect, useRef } from 'react';
+import{ useEffect, useRef } from 'react';
 import { Score } from '../../type';
-
-// export const ScoreBoard = () => {
-//   const [players, setPlayers] = useState<Player[]>(initialPlayers);
-//
-//
-//   const incrementScore = (id: number) => {
-//     const updatedPlayers = players.map((player) => {
-//       if (player.id === id) {
-//         return { ...player, score: player.score + 1 };
-//       }
-//       return player;
-//     });
-//     setPlayers(updatedPlayers);
-//   };
-//
-//   return (
-//     <div style={{ position: 'relative' }}>
-//       <div style={{ position: 'absolute', top: 0, left: 0, zIndex: 1 }}>
-//         <table>
-//           <tbody>
-//             {players.map((player) => (
-//               <tr key={player.id}>
-//                 <td>{player.score}</td>
-//                 <td>
-//                   <button onClick={() => incrementScore(player.id)}>+1</button>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   );
-// };
 
 export const ScoreBoard = ({score}: {score: Score})  => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -45,22 +11,22 @@ export const ScoreBoard = ({score}: {score: Score})  => {
     const ctx = canvas.getContext('2d');
     if (!ctx) return; // Context가 없으면 종료
 
-    //배경색을 빨강으로 설정
     ctx.fillStyle = 'green';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // 글꼴과 크기 설정
-    //
     const fontSize = 120;
     const fontType = 'Arial';
     ctx.font = `${fontSize}px ${fontType}`;
 
+    // 글자 중간 좌표
     const middleX1 = (canvas.clientWidth - ctx.measureText(`${score.p1}`).width) / 2;
     const middleX2 = (canvas.clientWidth - ctx.measureText(`${score.p2}`).width) / 2;
+
+    // 글자 높이는 가져오는 거 없어서 font size 바뀌면 하드코딩 해야함
     const baseHeight1 = 89;
     const baseHeight2 = canvas.clientHeight - 4;
     const offsetHeight = 60;
-    console.log(middleX1, middleX2);
+
     // 텍스트 렌더링 (x, y 좌표는 텍스트의 시작점)
     ctx.fillStyle = 'rgba(0, 255, 255, 0.5)';
     ctx.fillText(`${score.p1}`, middleX1, baseHeight1 + offsetHeight);
