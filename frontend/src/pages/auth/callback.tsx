701ce25fa7095dfc4637b9b5dcad732b3cb7c9d4
jimin <tesla52@naver.com>
2023-09-01 23:22:54 +0900

    feat: backend 에 맞게 redirect 적용. { redirectUrl: string } 으로 받아서 처리

--- frontend/src/pages/auth/callback.tsx
+++ frontend/src/pages/auth/callback.tsx
@@ -1,29 +1,12 @@
-import axios from 'axios';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import useRedirect from '../../context/useRedirect';
 
 const LoginCallback = () => {
   const router = useRouter();
-  // const [isClient, setIsClient] = useState(false)
-  // useEffect(() => {
-  //   setIsClient(true)
-  // }, [])
   const code = router.query.code;
 
-  useEffect(() => {
-    if (code) {
-      axios.get(`${process.env.NEXT_PUBLIC_API_URL}/auth/token?code=${code}`, { withCredentials: true })
-      .then((res) => {
-        console.log(res)
-        if (res.status === 200 && res.data.redirectUrl) {
-          router.push(res.data.redirectUrl);
-        }
-      })
-        .catch((error) => {
-          console.error('Login Redirect error:', error);
-        });
-    }
-  }, [code]);
+  const url =  code ? `${process.env.NEXT_PUBLIC_API_URL}/auth/token?code=${code}` : null;
+  useRedirect(router, url, 'get');
 
   return (
     <div>
