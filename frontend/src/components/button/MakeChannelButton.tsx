c7b9235b9c1f2eea7be7b869c9f9b0725fd98998
김민수 <minsu980107@naver.com>
2023-09-02 17:40:17 +0900

    36 chat channel UI (#42)
    * chore(chat): host -> owner, manager -> administrator
    
    * feat(chat): make 'make Channel' Layout
    
    * feat(chat): make message, pubilc-channel list UI

--- frontend/src/components/button/MakeChannelButton.tsx
+++ frontend/src/components/button/MakeChannelButton.tsx
@@ -1,27 +1,9 @@
 import styles from '@/styles/CreateChannel.module.css';
-import { socket } from '@/context/socket';
 
-const MakeChannelButton = () => {
-
-  const makeChannel = () => {
-    const title = prompt('방제를 입력하세요');
-    if (!title) return ;
-    const channelInfo = { title: title };
-
-    socket.emit('chat-channel-make', channelInfo);
-    console.log('makeCh channelInfo ', channelInfo);
-    // const channelPassword = prompt('채널 비밀번호를 입력하세요');
-    // if (!channelPassword) return ;
-    // socket.emit('channel-password', channelPassword);
-
-    // const channelRange = prompt('채널 범위를 입력하세요');
-    // if (!channelRange) return ;
-    // socket.emit('channel-range', channelRange);
-
-  };
+const MakeChannelButton = ({onClick}) => {
 
   return (
-    <button onClick={makeChannel} className={styles.MakeChannelButton}>make channel</button>
+    <button onClick={onClick} className={styles.MakeChannelButton}>make channel</button>
   );
 };
 
