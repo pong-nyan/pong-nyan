19c76fb833e443dcfa86291697095f6c10334a34
jimin <tesla52@naver.com>
2023-09-02 14:40:56 +0900

    fix(frontend): fix axios post argument input

--- frontend/src/context/usePostFetchData.ts
+++ frontend/src/context/usePostFetchData.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const usePostFetchData = (url: string, option?: object) => {
   const postFetchData = async () => {
     try {
-      const res = await axios.post(url, { withCredentials: true }, option);
+      const res = await axios.post(url, option, { withCredentials: true });
       return res.data.redirectUrl;
     } catch (error) {
       console.error('Fetch data error:', error);
