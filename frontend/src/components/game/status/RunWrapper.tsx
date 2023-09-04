9078c73a704c7f38baab8c0dc842815a18b3b9cb
yeonseong <yeonseong.dev@gmail.com>
2023-09-05 01:20:41 +0900

    feat(game): 가운데에 카운트다운 보여주기

--- frontend/src/components/game/status/RunWrapper.tsx
+++ frontend/src/components/game/status/RunWrapper.tsx
@@ -6,10 +6,13 @@ import CountDown from '../CountDown';
 const RunWrapper = ({ setGameStatus, playerNumber, opponentId, score, setScore}
     : { setGameStatus: Dispatch<SetStateAction<number>>, playerNumber: PlayerNumber, opponentId: string, score: Score, setScore: Dispatch<SetStateAction<Score>> } ) => {
   return (
-    <div>
-      <CountDown />
-      <Run setGameStatus={setGameStatus} playerNumber={playerNumber} opponentId={opponentId} score={score} setScore={setScore}/>
+    <div style={{ position: 'relative' }}>
+      <Run setGameStatus={setGameStatus} playerNumber={playerNumber} opponentId={opponentId} score={score} setScore={setScore} />
+      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+        <CountDown />
+      </div>
     </div>
+
   );
 };
 
