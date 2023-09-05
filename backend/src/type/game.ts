e82b078ede925aa2adc61ff1f5bbdf04de6cd865
minsuki2 <minsuki2@student.42seoul.kr>
2023-09-05 23:04:01 +0900

    feat(game): make QueueInfo type

--- backend/src/type/game.ts
+++ backend/src/type/game.ts
@@ -1,29 +1,38 @@
+import { Socket } from 'socket.io';
+
 export type PlayerNumber = 'player1' | 'player2';
 
 export type BallInfo = {
-    position: {
-        x: number;
-        y: number;
-    };
-    velocity: {
-        x: number;
-        y: number;
-    };
+  position: {
+    x: number;
+    y: number;
+  };
+  velocity: {
+    x: number;
+    y: number;
+  };
 }
 
 export type ScoreInfo = {
-    playerNumber: PlayerNumber;
-    loser: PlayerNumber;
+  playerNumber: PlayerNumber;
+  loser: PlayerNumber;
+}
+
+export type QueueInfo = {
+  client: Socket;
+  nickname: string;
 }
 
 export type GameInfo = {
-    roomName: string;
-    p1: string; // nickname
-    p2: string; // nickname
-    score: {
-        p1: number;
-        p2: number;
-    };
+  roomName: string;
+  player1: {
+    nickname: string;
+    score: number;
+  }
+  player2: {
+    nickname: string;
+    score: number;
+  }
 }
 
 export type RoomName = string;
