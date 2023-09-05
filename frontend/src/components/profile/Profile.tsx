8ab9fb79dd168119a0e488055400b01923254555
conteng <contemplation.person@gmail.com>
2023-09-05 16:19:39 +0900

    feat: add Profile component

---
+++ frontend/src/components/profile/Profile.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import { ProfileProps } from '@/type';
+
+const Profile = ({ nickname, profileImage, laderBoardScore, recentPerformance, achievement }: ProfileProps) => {
+  return (
+    <div>
+      <Image src={profileImage} alt={nickname} width={100} height={100} />
+      <h2>{nickname}</h2>
+      <h3>래더 스코어</h3>
+      <p>{laderBoardScore}</p>
+      <h3>래더 최근 성적</h3>
+      <p>{recentPerformance}</p>
+      <h3>업적</h3>
+      <p>{achievement}</p>
+    </div>
+  );
+};
+
+Profile.defaultProps = {
+  profileImage: '/pongNyan.png',
+  nickname: 'nickname',
+  laderBoardScore: 0,
+  recentPerformance: 'empty',
+  achievement: 'empty',
+};
+
+export default Profile;
