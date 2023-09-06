63ef8418d165f46fb1c0b69771aac043cb7e44c4
김민수 <minsu980107@naver.com>
2023-09-06 14:05:13 +0900

    feat(chat): 대화방 내용 컴포넌트로 바꾸기

--- frontend/src/pages/channel/message-channel-list.tsx
+++ frontend/src/pages/channel/message-channel-list.tsx
@@ -1,3 +1,4 @@
+import ParticipatingChannelList from '@/components/chat/ParticipatingChannelList';
 import React, { useState } from 'react';
 
 // TODO: 유저가 참여한 방목록을 보여줄 유저의 구별이 구현되어 있지 않음 유저를 받을수 있게 되면 다시 만들어야함
@@ -15,12 +16,13 @@ const MessageChannelListPage = () => {
           <h3 style={{ margin: '0' }}>참여방 목록</h3>
           <button style={{ marginLeft: '10px' }}>참여방보기</button>
         </div>
-        {showJoinedChannels && (
-          <div>
-            {/* TODO: 유저가 참여한 방 목록 보여주기 */}
-            <p>실제 목록</p>
-          </div>
-        )}
+        <>
+          {showJoinedChannels && (
+            <>
+              <ParticipatingChannelList />
+            </>
+          )}
+        </>
       </div>
       <div>
         <div style={{ display: 'flex', alignItems: 'center' }} onClick={() => setShowDMs(!showDMs)}>
@@ -28,10 +30,9 @@ const MessageChannelListPage = () => {
           <button style={{ marginLeft: '10px' }}>참여방보기</button>
         </div>
         {showDMs && (
-          <div>
-            {/* TODO: 유저의 DM 목록 보여주기 */}
-            <p>실제 목록</p>
-          </div>
+          <>
+            <ParticipatingChannelList />
+          </>
         )}
       </div>
     </div>
