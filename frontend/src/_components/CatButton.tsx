70cc5022379e48e69dd1de6bb615fb377e76b310
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-09 08:31:03 +0900

    refactor(frontend): 공통으로 사용할 component 분리

--- frontend/src/_components/CatButton.tsx
+++ frontend/src/_components/CatButton.tsx
@@ -2,20 +2,23 @@ import Image from 'next/image';
 
 type CatButtonProps = {
     onClickFunction: () => void;
+    label: string;
     width: number;
     height: number;
 };
 
-const CatButton = ({ onClickFunction, width, height }: CatButtonProps) => {
+const CatButton = ({ onClickFunction, label, width, height }: CatButtonProps) => {
   return (
     <button>
       <Image src="/assets/cat-footprint.png" alt="catFootprint" onClick={onClickFunction} width={width ?? 42} height={height ?? 42} />
+      {label}
     </button>
   );
 };
 
 CatButton.defaultProps = {
   onClickFunction: () => { console.error('onClickFunction is not defined');},
+  label: 'button',
   width: 42,
   height: 42,
 };
