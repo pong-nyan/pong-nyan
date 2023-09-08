9a0922c8ed7ba1427ab3759cf5e96f4260c0f008
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-09 08:23:35 +0900

    feat(game): add Rank, Origin option

--- frontend/src/pages/game/index.tsx
+++ frontend/src/pages/game/index.tsx
@@ -6,7 +6,10 @@ import { useState } from 'react';
 
 enum GameStatus {
 	Start,
-	Run,
+	RankPnRun,
+  NormalPnRun,
+  RankOriginRun,
+  NormalOriginRun,
 	End
 }
 
@@ -21,7 +24,14 @@ export default function Game(): JSX.Element {
   switch (gameStatus) {
   case GameStatus.Start:
     return (<Start setGameStatus={setGameStatus} setPlayerNumber={setPlayerNumber} setOpponentId={setOpponentId} />);
-  case GameStatus.Run:
+  // TODO: 각  모드에 맞게  수정  필요
+  case GameStatus.RankPnRun:
+    return (<RunWrapper setGameStatus={setGameStatus} playerNumber={playerNumber} opponentId={opponentId} score={score} setScore={setScore}/>);
+  case GameStatus.NormalPnRun:
+    return (<RunWrapper setGameStatus={setGameStatus} playerNumber={playerNumber} opponentId={opponentId} score={score} setScore={setScore}/>);
+  case GameStatus.RankOriginRun:
+    return (<RunWrapper setGameStatus={setGameStatus} playerNumber={playerNumber} opponentId={opponentId} score={score} setScore={setScore}/>);
+  case GameStatus.NormalOriginRun:
     return (<RunWrapper setGameStatus={setGameStatus} playerNumber={playerNumber} opponentId={opponentId} score={score} setScore={setScore}/>);
   case GameStatus.End:
     return (<End setGameStatus={setGameStatus} setPlayerNumber={setPlayerNumber} />);
