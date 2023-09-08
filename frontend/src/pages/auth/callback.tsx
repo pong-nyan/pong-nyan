2e6173c9465b02b6c68a258f7e80a1c4d20ca0fa
yeonseong <yeonseong.dev@gmail.com>
2023-09-09 00:00:37 +0900

    !style: useGetFetchData -> useGetFetchDataRedirect

--- frontend/src/pages/auth/callback.tsx
+++ frontend/src/pages/auth/callback.tsx
@@ -1,14 +1,14 @@
 import { useRouter } from 'next/router';
 import useRedirect from '../../context/useRedirect';
 import { useEffect, useState } from 'react';
-import { useGetFetchData } from '../../context/useGetFetchData';
+import { useGetFetchDataRedirect } from '../../context/useGetFetchData';
 
 const LoginCallback = () => {
   const router = useRouter();
   const code = router.query.code;
   const [redirectUrl, setRedirectUrl] = useState<string | null>(null);
   const url =  code ? `${process.env.NEXT_PUBLIC_API_URL}/auth/token?code=${code}`: null;
-  const getFetchData = useGetFetchData(url);
+  const getFetchData = useGetFetchDataRedirect(url);
 
   useEffect(() => {
     const fetchData = async () => {
