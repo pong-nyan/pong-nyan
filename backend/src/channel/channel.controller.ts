775a982a314bf896422497c2f594287762e5590b
김민수 <minsu980107@naver.com>
2023-09-09 14:49:44 +0900

    feat(chat): Controller에 @UseGuards추가

--- backend/src/channel/channel.controller.ts
+++ backend/src/channel/channel.controller.ts
@@ -2,9 +2,12 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ChannelService } from './channel.service';
 import { Channel } from '../type/channel';
+import { ChannelGuard } from './channel.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Controller('channel')
 @ApiTags('channel')
+@UseGuards(ChannelGuard)
 export class ChannelController {
   constructor(private readonly channelService: ChannelService) {
   }
@@ -13,6 +16,5 @@ export class ChannelController {
   getPublicChannels(): Channel[] {
     return this.channelService.getPublicChannels();
   }
-
 
 }
