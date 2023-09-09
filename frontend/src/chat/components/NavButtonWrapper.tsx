487d269f21e92ca51b447c140dc2569836c31031
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-09 20:55:41 +0900

    fix(frontend): not showing button image in safari
    see : https://stackoverflow.com/questions/68526892/nextjs-image-does-not-show-on-safari

--- frontend/src/chat/components/NavButtonWrapper.tsx
+++ frontend/src/chat/components/NavButtonWrapper.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 const HomeButton = () => {
   return (
-    <button>
-      <Link href="/" style={{ position: 'relative', width: '100%', height: '100%' }}>
+    <button style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <Link href="/">
         <Image src="/assets/home-button.png" alt="home-button" fill style={{ objectFit: 'contain' }} />
       </Link>
     </button>
@@ -15,9 +15,9 @@ const HomeButton = () => {
 //TODO: mikim3에게 묻고 수정-> list와 message-channel-list의 차이점
 const ListButton = () => {
   return (
-    <button>
-      <Link href="/channel/list" style={{ position: 'relative', width: '100%', height: '100%' }}>
-        <Image src="/assets/list-button.png" alt="list-button" fill style={{ objectFit: 'contain' }} />
+    <button style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <Link href="/channel/list">
+        <Image src="/assets/list-button.png" alt="list-button" fill style={{ objectFit: 'contain'}} />
       </Link>
     </button>
   );
@@ -26,8 +26,8 @@ const ListButton = () => {
 //TODO: mikim3에게 링크 묻고 수정 -> list와 message-channel-list의 차이점
 const PublicChannelListButton = () => {
   return (
-    <button>
-      <Link href="/channel/public-channel-list" style={{ position: 'relative', width: '100%', height: '100%' }}>
+    <button style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <Link href="/channel/public-channel-list">
         <Image src="/assets/public-room-button.png" alt="public-room-button" fill style={{ objectFit: 'contain' }} />
       </Link>
     </button>
@@ -36,9 +36,9 @@ const PublicChannelListButton = () => {
 
 const GameButton = () => {
   return (
-    <button>
-      <Link href="/game" style={{ position: 'relative', width: '100%', height: '100%' }}>
-        <Image src="/assets/game-button.png" alt="game-button" fill style={{ objectFit: 'contain' }} />
+    <button style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <Link href="/game">
+        <Image src="/assets/game-button.png" alt="game-button" fill style={{ objectFit: 'contain', zIndex: 10}} />
       </Link>
     </button>
   );
