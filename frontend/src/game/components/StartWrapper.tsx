d6d39fa3cd004ad4e71bc1e90fe87c1d73a88609
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-09 10:44:26 +0900

    style(game): clean import

--- frontend/src/game/components/StartWrapper.tsx
+++ frontend/src/game/components/StartWrapper.tsx
@@ -1,10 +1,8 @@
-import { useContext, Dispatch, SetStateAction } from 'react';
+import { useContext } from 'react';
 import CatButton from '@/_components/CatButton';
 import { SocketContext } from '@/context/socket';
 
-const GameStartWrapper = ({ setGameStatus, setPlayerNumber, setOpponentId }
-  : { setGameStatus: Dispatch<SetStateAction<number>>, setPlayerNumber: Dispatch<SetStateAction<PlayerNumber | undefined>>, setOpponentId: Dispatch<SetStateAction<string | undefined>>}
-) => {
+const GameStartWrapper = () => {
   const socket = useContext(SocketContext);
 
   <CatButton onClickFunction={() => {
