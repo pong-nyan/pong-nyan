6a06f0cf4d9735934228042a54ed437ea17b5ad6
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-09 11:09:55 +0900

    style(game): GameStatus  enum 사용

--- frontend/src/game/components/Start.tsx
+++ frontend/src/game/components/Start.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, SetStateAction, useContext, useState } from 'react';
 import { SocketContext } from '@/context/socket';
-import { PlayerNumber } from '@/game/gameType';
+import { PlayerNumber, GameStatus } from '@/game/gameType';
 import GameStartWrapper from './GameStartWrapper';
 
 export default function Start({ setGameStatus, setPlayerNumber, setOpponentId }
@@ -8,6 +8,7 @@ export default function Start({ setGameStatus, setPlayerNumber, setOpponentId }
   const socket = useContext(SocketContext);
   const [ loading, setLoading ] = useState(false);
 
+  // TODO: 추후 rank - origin, normal - origin 의 경우의 수에 따라 socket.on 을 추가해야 함
   socket.on('game-randomStart-rank-pn', ({player1Id, player2Id}: {player1Id: string, player2Id: string}) => {
     if (socket.id === player1Id) { 
       setPlayerNumber('player1');
@@ -17,7 +18,7 @@ export default function Start({ setGameStatus, setPlayerNumber, setOpponentId }
       setPlayerNumber('player2');
       setOpponentId(player1Id);
     }
-    setGameStatus(1);
+    setGameStatus(GameStatus.RankPnRun);
   });
   socket.on('game-loading', () => {
     setLoading(true);
