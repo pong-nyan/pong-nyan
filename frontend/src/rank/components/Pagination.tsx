c70bad1a80985147343e7934dd4620b56d83bbd0
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-09 22:19:42 +0900

    feat(rank): pagination css

--- frontend/src/rank/components/Pagination.tsx
+++ frontend/src/rank/components/Pagination.tsx
@@ -1,12 +1,13 @@
 import { PaginationProps } from '../../type/rankType';
+import styles from '@/rank/styles/Rank.module.css';
 
 const Pagination = ({ currentPage, handleCurrentPage}: PaginationProps) => {
   return (
-    <>
+    <div className={styles.pagination}>
       <button onClick={() => handleCurrentPage(currentPage - 1)}>이전</button>
       {currentPage}
       <button onClick={() => handleCurrentPage(currentPage + 1)}>다음</button>
-    </>
+    </div>
   );
 };
 
