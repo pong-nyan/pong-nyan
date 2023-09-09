8a16266dbf5ee99fd289f02a783b4911d16fad98
김민수 <minsu980107@naver.com>
2023-09-09 16:52:43 +0900

    style: hot fix type folder error

---
+++ backend/type/chatType.ts
@@ -0,0 +1,28 @@
+declare module 'socket.io' {
+  interface Socket {
+    intraId?: string;
+  }
+}
+
+/*
+* shared between frontend and backend
+* below type
+*/
+
+export type ChannelType = 'public' | 'private' | 'protected';
+
+export type Channel = ChannelInfo & {
+  id: string,
+  owner: string,
+  administrator: string[],
+  userList: string[]
+  invitedUsers: string[]
+}
+
+export type ChannelInfo = {
+  title: string,
+  password?: string,
+  channelType: 'public' | 'private' | 'protected',
+  maxUsers: number,
+}
+
---
+++ backend/type/userType.ts
@@ -0,0 +1,15 @@
+import { RoomName } from './socketType';
+
+/*
+* shared between frontend and backend
+* below type
+*/
+
+export type IntraId = number;
+
+export type UserInfo = {
+    intraId: number,
+    nickname: string,
+    chatRoomList: RoomName[],
+    gameRoom: RoomName
+}
