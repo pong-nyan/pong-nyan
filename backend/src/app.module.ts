8c194486fd97c84d105cab55a5aaa80ec299b675
conteng <68673565+contemplation-person@users.noreply.github.com>
2023-09-09 21:06:10 +0900

    97 백엔드 데이터를 받아서 랭킹 정보를 프로필에 띄우기 (#134)
    * chore: wip: test
    
    * style: delete tester
    
    * style: 같은 기능을 가진 함수 통합.
    
    * style: delete tester
    
    * feat: profile page proto
    
    * feat: profile page

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,9 +10,10 @@ import { AppGateway } from './app.gateway';
 import { ChannelModule } from './chat/channel.module';
 import { UserService } from './user.service';
 import { RankModule } from './rank/rank.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GameModule, AuthModule, DatabaseModule, ChatModule, ChannelModule, RankModule],
+  imports: [ConfigModule.forRoot(), GameModule, AuthModule, DatabaseModule, ChatModule, ChannelModule, RankModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService, AppGateway, UserService],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -7,86 +7,82 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>,
-                private readonly jwtService: JwtService
-    ) { }
-    async getToken(code: string) {
-        //  post request oauth2 token
-            const ret = await axios.post('https://api.intra.42.fr/oauth/token',
-                {
-                    client_id: process.env.CLIENT_ID,
-                    client_secret: process.env.CLIENT_SECRET,
-                    code: code,
-                    grant_type: 'authorization_code',
-                    redirect_uri: process.env.REDIRECT_URI
-                });
-            return ret.data;
-    }
-    async getFtUserInfo(accessToken: string) {
-        //  get request user info
-        const ret = await axios.get('https://api.intra.42.fr/v2/me', {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-        return ret;
-    }
+  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService
+  ) { }
+  async getToken(code: string) {
+    //  post request oauth2 token
+    const ret = await axios.post('https://api.intra.42.fr/oauth/token',
+      {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code: code,
+        grant_type: 'authorization_code',
+        redirect_uri: process.env.REDIRECT_URI
+      });
+    return ret.data;
+  }
 
-    // get access token from cookie
-    async getUserInfoFromToken(accessToken: string) {
-        //  get user info from 42 api
-        const ftUserInfo = await this.getFtUserInfo(accessToken);
-        const { id: intraId, login: intraNickname } = ftUserInfo.data;
-        return { intraId, intraNickname };
-    }
+  async getFtUserInfo(accessToken: string) {
+    //  get request user info
+    const ret = await axios.get('https://api.intra.42.fr/v2/me', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+    return ret;
+  }
 
-    async getUserInfoFromOurDB(intraId: number) {
-        if (!intraId) return null;
-        return await this.userRepository.findOne({ where: { intraId: intraId } });
-    }
+  // get access token from cookie
+  async getUserInfoFromToken(accessToken: string) {
+    //  get user info from 42 api
+    const ftUserInfo = await this.getFtUserInfo(accessToken);
+    const { id: intraId, login: intraNickname } = ftUserInfo.data;
+    return { intraId, intraNickname };
+  }
 
-    async findUser(intraId: number) {
-        if (!intraId) return null;
-        return await this.userRepository.findOne({ where: { intraId: intraId } });
-    }
+  async findUser(intraId: number) {
+    if (!intraId) return null;
+    return await this.userRepository.findOne({ where: { intraId: intraId } });
+  }
 
-    async isUserRegisterdQR(intraId: number) {
-        if (!intraId) return false;
-        const user = await this.userRepository.findOne({ where: { intraId: intraId } });
-        if (!user) return false;
-        return user.google2faEnable;
-    }
-    /**
-     * @warning this function also updates user info
-     * @warning should be called after check if user exists
-    */
-    async createUser(intraId: number, intraNickname: string, nickname: string, avatar: string, rankScore: number, email: string) {
-        const user = new User();
-        user.intraId = intraId;
-        user.intraNickname = intraNickname;
-        user.nickname = nickname;
-        user.avatar = avatar;
-        user.rankScore = rankScore;
-        user.email = email;
-        const existUser = this.userRepository.findOne({ where: { intraId: intraId } });
-        if (!existUser) return await this.userRepository.update({ intraId: intraId }, user);
-        else return await this.userRepository.save(user);
-    }
+  async isUserRegisterdQR(intraId: number) {
+    if (!intraId) return false;
+    const user = await this.userRepository.findOne({ where: { intraId: intraId } });
+    if (!user) return false;
+    return user.google2faEnable;
+  }
+  /**
+   * @warning this function also updates user info
+   * @warning should be called after check if user exists
+  */
+  async createUser(intraId: number, intraNickname: string, nickname: string, avatar: string, rankScore: number, email: string) {
+    const user = new User();
+    user.intraId = intraId;
+    user.intraNickname = intraNickname;
+    user.nickname = nickname;
+    user.avatar = avatar;
+    user.rankScore = rankScore;
+    user.email = email;
+    const existUser = this.userRepository.findOne({ where: { intraId: intraId } });
+    if (!existUser) return await this.userRepository.update({ intraId: intraId }, user);
+    else return await this.userRepository.save(user);
+  }
 
-    async createJwt(intraId: number, intraNickname: string, nickname: string) {
-        const payload = {intraId, intraNickname, nickname};
-        return await this.jwtService.signAsync(payload);
-    }
+  async createJwt(intraId: number, intraNickname: string, nickname: string) {
+    const payload = { intraId, intraNickname, nickname };
+    return await this.jwtService.signAsync(payload);
+  }
 
-    async updateUser2faSecret(intraId: number, secret: string) {
-        return await this.userRepository.update({intraId: intraId}, {google2faSecret: secret});
-    }
+  async updateUser2faSecret(intraId: number, secret: string) {
+    return await this.userRepository.update({ intraId: intraId }, { google2faSecret: secret });
+  }
 
-    async updateUser2faEnable(intraId: number, enable: boolean) {
-        return this.userRepository.update({intraId: intraId}, {google2faEnable: enable});
-    }
+  async updateUser2faEnable(intraId: number, enable: boolean) {
+    return this.userRepository.update({ intraId: intraId }, { google2faEnable: enable });
+  }
 
-    async enableTwoFactorAuthentication(user: User) {
-        await this.updateUser2faEnable(user.intraId, true);
-    }
+  async enableTwoFactorAuthentication(user: User) {
+    await this.updateUser2faEnable(user.intraId, true);
+  }
 }
