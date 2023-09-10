54b90b7b2a540ec2e90d40a57456bfefea5eb3ab
minsuki2 <minsuki2@student.42seoul.kr>
2023-09-10 21:49:44 +0900

    feat(frotend): socket emit data 없이 보내기

--- frontend/src/auth/components/Google2FA.tsx
+++ frontend/src/auth/components/Google2FA.tsx
@@ -1,24 +1,27 @@
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import useNotAuth from '@/context/useNotAuth';
-import { socket } from '@/context/socket';
+import { SocketContext } from '@/context/socket';
+import { useContext } from 'react';
 
 const Google2FA = () => {
   useNotAuth();
+  const socket = useContext(SocketContext);
   const router = useRouter();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit =  async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const code = e.currentTarget.code.value;
-    axios.post(`${process.env.NEXT_PUBLIC_API_URL}/google2fa/enable`, { code }, { withCredentials: true })
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/google2fa/enable`, { code }, { withCredentials: true })
       .then((res) => {
         if (res.status === 202) {
           localStorage.setItem('user', JSON.stringify(res.data));
-          socket.emit('auth-set-map', { intraId : res.data.intraId });
-          router.replace('/');
+          console.log('Emitting auth-set-map');
+          socket.emit('auth-set-map');
+          router.push('/');
         }
-      }
-      ).catch((err) => {
+      })
+      .catch((err) => {
         if (err.response.status === 401) {
           alert('Enable failed. Please try again.');
         }
