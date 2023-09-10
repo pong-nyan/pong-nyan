bbd9b1635842dcac4ce59611793664adfef148fe
minsuki2 <minsuki2@student.42seoul.kr>
2023-09-10 15:54:13 +0900

    hotfix(frontend): endClientId

--- frontend/src/auth/logic/redirectOauth.ts
+++ frontend/src/auth/logic/redirectOauth.ts
@@ -4,7 +4,7 @@ const redirect42Oauth = (router: NextRouter) => {
   const envClientId = process.env.NEXT_PUBLIC_CLIENT_ID;
   const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI;
   router.push(
-    `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`
+    `https://api.intra.42.fr/oauth/authorize?client_id=${envClientId}&redirect_uri=${redirect_uri}&response_type=code`
   );
 };
 
