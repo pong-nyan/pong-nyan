52a0e4b390f263a1f08395e5ad1aa094ed0f8e8a
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-10 21:56:56 +0900

    wip(friend): 기본 파일들 구성

---
+++ frontend/src/friend/components/Manage.tsx
@@ -0,0 +1,11 @@
+const Manage = () => {
+  return (
+    <div>
+      <h1>Friend Manage</h1>
+      <h2>accpet or reject</h2>
+      <h2>request friend</h2>
+    </div>
+  );
+};
+
+export default Manage;
---
+++ frontend/src/pages/friend/manage.tsx
@@ -0,0 +1,12 @@
+import Manage from '@/friend/components/Manage';
+
+const FriendManagePage = () => {
+  return (
+    <div>
+      <h1>Friend Manage</h1>
+      <Manage />
+    </div>
+  );
+};
+
+export default FriendManagePage;
