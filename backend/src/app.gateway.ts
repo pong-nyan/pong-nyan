30bb07e9ca6dae66e0a6da5a57580be48133e38e
minsuki2 <minsuki2@student.42seoul.kr>
2023-09-10 13:46:28 +0900

    chore: makefile, eslint 수정

--- backend/src/app.gateway.ts
+++ backend/src/app.gateway.ts
@@ -25,10 +25,7 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   async handleConnection(@ConnectedSocket() client: Socket) {
     console.log('AppGateway Connection', client.id);
-    if (!this.userService.checkPnJwt(client))
-    {
-      return ;
-    }
+    if (!this.userService.checkPnJwt(client)) return ;
     console.log('AppGateway Connection, befor auth-set-map-payload client.id', client.id);
     this.server.to(client.id).emit('auth-set-map-payload');
     console.log('AppGateway Connection, idMap', this.userService.idMap);
@@ -36,7 +33,7 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   async handleDisconnect(client: Socket) {
     console.log('AppGateway Disconnection client.id', client.id);
-    console.log('AppGateway Disconnection', this.userService.getIntraId(client.id));
+    console.log('AppGateway Disconnection', this.userService.idMap);
     this.userService.deleteIdMap(client.id);
   }
 }
