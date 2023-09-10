2604df97d2ebd7f22fa952eefdb164ca11b51d18
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-10 13:02:44 +0900

    refactor(login): redirectOauth 함수 분리

---
+++ frontend/src/auth/logic/redirectOauth.ts
@@ -0,0 +1,11 @@
+import { NextRouter } from 'next/router';
+
+const redirectOauth = (router: NextRouter) => {
+  const client_id = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI;
+  router.push(
+    `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`
+  );
+};
+
+export default redirectOauth;
