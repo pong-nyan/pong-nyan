3b8d425553b92b4bf3ea0155555e8eaf7860853e
yeonseong <yeonseong.dev@gmail.com>
2023-09-10 22:41:21 +0900

    feat(frontend): 친구요청 뷰 뼈대 완성

---
+++ frontend/src/friend/components/RequestFrien.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+const RequestFriend = () => {
+  const [nickname, setNickname] = useState('');
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!nickname) {
+      alert('친구요청할 친구의 nickname을 입력해주세요.');
+      return;
+    }
+    // TODO: request friend using axios
+  };
+
+  return (
+    <>
+      <h1>Request Friend</h1>
+      <form onSubmit={handleSubmit}>
+        <label>친구요청할 친구의 nickname</label>
+        <input
+          type='text'
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+        <button type='submit'>친구요청</button>
+      </form>
+    </>
+  );
+};
+
+export default RequestFriend;
