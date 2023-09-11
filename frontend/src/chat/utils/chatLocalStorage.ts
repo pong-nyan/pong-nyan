490552939197a7a248ea071d75ac0e920613a531
김민수 <minsu980107@naver.com>
2023-09-11 16:33:12 +0900

    feat(chat): 메시지를 lcalStorage에 저장하는 로직구현및 파일로분리

---
+++ frontend/src/chat/utils/chatLocalStorage.ts
@@ -0,0 +1,13 @@
+export function saveMessageToLocalStorage(channelId: string, message: string) {
+  const key = `channel-${channelId}-messages`;
+  const storedMessages = localStorage.getItem(key);
+  const existingMessages = storedMessages ? JSON.parse(storedMessages) : [];
+  existingMessages.push(message);
+  localStorage.setItem(key, JSON.stringify(existingMessages));
+}
+
+export function getMessagesFromLocalStorage(channelId: string) {
+  const key = `channel-${channelId}-messages`;
+  const storedMessages = localStorage.getItem(key);
+  return storedMessages ? JSON.parse(storedMessages) : [];
+}
