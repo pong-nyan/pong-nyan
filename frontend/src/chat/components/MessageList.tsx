09edb9c00662b6c79916a225b3fbbe29b0720a36
rlaalstn0107 <minsu980107@naver.com>
2023-09-12 00:32:43 +0900

    feat(chat): 링크로 채팅창에서 profile로 보내기

--- frontend/src/chat/components/MessageList.tsx
+++ frontend/src/chat/components/MessageList.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Message } from '@/type/chatType';
+import Link from 'next/link';
 
 function MessageList({ messages }: { messages: Message[] }) {
   return (
     <div>
       {messages.map((message, index) => (
         <div key={index}>
-          {/* TODO: 여기에서 유저 아이디를 profile 컴포넌트로 넘겨줌 */}
-          <strong>{message.nickname}</strong>
+          <Link href={`/profile/${message.nickname}`}>
+            <strong>{message.nickname}</strong>
+          </Link>
           <p>{message.content}</p>
         </div>
       ))}
