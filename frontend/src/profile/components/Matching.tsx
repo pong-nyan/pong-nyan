df34633ed7c03cb8a940e907a3a178379efaca8b
yeonseong <yeonseong.dev@gmail.com>
2023-09-12 00:51:02 +0900

    feat(frontend): 자기자신과 매칭 막기

--- frontend/src/profile/components/Matching.tsx
+++ frontend/src/profile/components/Matching.tsx
@@ -1,6 +1,21 @@
+import { useContext } from 'react';
+import { SocketContext } from '@/context/socket';
+
 const Matching = ({ nickname }: { nickname: string}) => {
+  const socket = useContext(SocketContext);
   const startMatching = () => {
+    const user = localStorage.getItem('user');
+    if (!user) {
+      alert('로그인을 해주세요.');
+      return;
+    }
+    const myNickname = JSON.parse(user).nickname;
+    if (myNickname === nickname) {
+      alert('자기자신에게는 매칭을 할 수 없습니다.');
+      return;
+    }
     alert(`${nickname} 에게 게임신청, 상대도 매칭시작을 눌러야 게임을시작합니다.`);
+    socket.emit('game-friendStart', { nickname });    
   };
 
   return (
