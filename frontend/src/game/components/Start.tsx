5ca2e177f66ef9b3d116232d3845ac39242b1058
minsuki2 <minsuki2@student.42seoul.kr>
2023-09-12 23:59:44 +0900

    feat(game): Start, End socketOn 구현

--- frontend/src/game/components/Start.tsx
+++ frontend/src/game/components/Start.tsx
@@ -1,16 +1,21 @@
-import { Dispatch, SetStateAction, useContext, useState, useEffect } from 'react';
+import { Dispatch, SetStateAction, useState, useEffect } from 'react';
 import { PlayerNumber, GameStatus } from '@/type/gameType';
 import GameStartWrapper from './GameStartWrapper';
 import { socketOnGameStartEvent, socketOnGameLoadingEvent } from '@/context/socketGameEvent';
 import { SocketId } from '@/type/socketType';
 
-export default function Start({ gameStatus, setGameStatus, setPlayerNumber, setOpponentId }
-  : { gameStatus: GameStatus, setGameStatus: Dispatch<SetStateAction<number>>, setPlayerNumber: Dispatch<SetStateAction<PlayerNumber>>, setOpponentId: Dispatch<SetStateAction<SocketId>>}) {
+const Start = ({ gameStatus, setGameStatus, setPlayerNumber, setOpponentId }: {
+  gameStatus: GameStatus, 
+  setGameStatus: Dispatch<SetStateAction<number>>, 
+  setPlayerNumber: Dispatch<SetStateAction<PlayerNumber>>, 
+  setOpponentId: Dispatch<SetStateAction<SocketId>>
+  }) => {
   const [ loading, setLoading ] = useState(false);
 
-  // TODO: 추후 rank - origin, normal - origin 의 경우의 수에 따라 socket.on 을 추가해야 함
-  socketOnGameStartEvent(setGameStatus, setPlayerNumber, setOpponentId);
-  socketOnGameLoadingEvent(setLoading);
+  useEffect(() => {
+    socketOnGameStartEvent(setGameStatus, setPlayerNumber, setOpponentId);
+    socketOnGameLoadingEvent(setLoading);
+  }, [setGameStatus, setPlayerNumber, setOpponentId]);
 
   return (
     loading ?
@@ -18,3 +23,5 @@ export default function Start({ gameStatus, setGameStatus, setPlayerNumber, setO
       <GameStartWrapper gameStatus={gameStatus}/>
   );
 }
+
+export default Start;
