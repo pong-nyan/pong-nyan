3d287a57f5ff90da590bbc10711f17f854850a7f
juha <contemplation.person@gmail.com>
2023-09-14 04:11:43 +0900

    fix: socket 이  namespace가 없이 실행됨

--- frontend/src/profile/components/Matching.tsx
+++ frontend/src/profile/components/Matching.tsx
@@ -15,7 +15,7 @@ const Matching = ({ nickname }: { nickname: string}) => {
       return;
     }
     alert(`${nickname} 에게 게임신청, 상대도 매칭시작을 눌러야 게임을시작합니다.`);
-    socket.emit('game-friendStart', { nickname });    
+    socket.gameNamespace.emit('game-friendStart', { nickname });    
   };
 
   return (
