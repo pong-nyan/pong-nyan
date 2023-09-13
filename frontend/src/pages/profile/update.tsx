760997c36bdd45fb1ff77b46ea3fed8f3299f6b6
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-14 02:33:04 +0900

    feat(profile): 내정보 수정 가능

---
+++ frontend/src/pages/profile/update.tsx
@@ -0,0 +1,11 @@
+import UpDateMyInfo from '@/profile/components/UpdateMyInfo';
+
+const UpdatePage = () => {
+  return (
+    <div>
+      <UpDateMyInfo />
+    </div>
+  );
+};
+
+export default UpdatePage;
