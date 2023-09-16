285d84640458a9ded8d78e6521e8437c1d5737af
minsuki2 <minsuki2@student.42seoul.kr>
2023-09-16 19:54:09 +0900

    feat(backend): app controller userInfo 가져오기

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
+import { UserInfo } from './type/userType';
+import { PnJwtPayload, PnPayloadDto } from 'src/dto/pnPayload.dto';
+import { Controller2faGuard } from 'src/guard/controller2fa.guard';
 
+@UseGuards(Controller2faGuard)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+              private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/user/me')
+  getUserInfo(@PnJwtPayload() pnPayload: PnPayloadDto): UserInfo {
+    return this.userService.getUserInfo(pnPayload.intraId);
   }
 }
