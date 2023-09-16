e4fcd53012d69fe3ea0c430640e3063159c9a3e9
jimin <tesla52@naver.com>
2023-09-16 17:23:02 +0900

    feat(frontend): make socketChatCreateDmEvent

--- frontend/src/context/socketChatEvent.ts
+++ frontend/src/context/socketChatEvent.ts
@@ -1,16 +1,17 @@
 import { chatNamespace } from '@/context/socket';
 import Router from 'next/router';
 import { Nickname } from '@/type/userType';
+import { Channel } from '@/type/chatType';
 
 export const socketEmitChatCreateDmEvent = (nickname: Nickname) => {
   chatNamespace.emit('chat-create-dm', { nickname });
-}
+};
 
+export const socketOnChatJoinDmEvent = () => { 
+  chatNamespace.on('chat-create-dm', ( {channel}: { channel: Channel }) => {
+    // setChannelList()
+    
+    Router.push(`/chat/${channel.id}`);
+  });
+};
 
-// export const socketOnChatJoinDmEvent = () => { 
-//   chatNamespace.on('chat-join-dm', ({ } : ) => {
-//     console.log(data);
-//     Router.push(`/chat/${data.channelId}`);
-//   });
-// }
-//
