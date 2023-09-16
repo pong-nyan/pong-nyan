94f871393be1c2f7f4d9978c0027d9b9827c826e
김민수 <minsu980107@naver.com>
2023-09-16 23:30:37 +0900

    한글 사이트 표준지키기

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en"> 
-      <Head> 
-        <link rel="icon" href="/pong-nyan.png"/> 
+    <Html lang="ko">
+      <Head>
+        <link rel="icon" href="/pong-nyan.png"/>
       </Head>
       <body>
         <Main />
