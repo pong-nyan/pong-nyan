b7c8b2824b7c12e39c7753e969ee8f4b5141c251
minsuki2 <minsuki2@student.42seoul.kr>
2023-09-16 23:42:49 +0900

    hotfix: block 401 에러 막기

--- frontend/src/context/socketChatEvent.ts
+++ frontend/src/context/socketChatEvent.ts
@@ -19,11 +19,20 @@ export const socketOnChatJoinDmEvent = () => {
 export const socketOnChatNewMessage = () => { 
   chatNamespace.on('chat-new-message', async ({ message, channelId }: { message: Message, channelId: ChannelId }) => {
     // 메시지를 받은 채널 ID
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user/me`);
-    const userBlockList = res.data.userBlockList;
-    if (userBlockList && userBlockList.includes(message.nickname)) return;
-    addMessageToLocalStorage(channelId, message);
-    chatNamespace.emit('chat-watch-new-message', { channelId });
+    try {
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user/me`);
+      const userBlockList = res.data.userBlockList;
+      if (userBlockList && userBlockList.includes(message.nickname)) return;
+      addMessageToLocalStorage(channelId, message);
+      chatNamespace.emit('chat-watch-new-message', { channelId });
+    } catch (err: any) {
+      if (err?.response?.status === 401) {
+        alert('로그인이 필요합니다.');
+        localStorage.removeItem('user');
+        location.replace('/auth');
+        return ;
+      }
+    }
   });
 };
 
