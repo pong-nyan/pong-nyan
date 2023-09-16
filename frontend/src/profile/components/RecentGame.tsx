b06a0ace4c4ef462ead9916e464930642b861870
YeonSeong-Lee <yeonseong.dev@gmail.com>
2023-09-16 14:39:40 +0900

    feat: profile css

--- frontend/src/profile/components/RecentGame.tsx
+++ frontend/src/profile/components/RecentGame.tsx
@@ -1,14 +1,16 @@
 import { Game, GameModeEnum } from '@/type/profileType';
+import styles from '@/profile/styles/RecentGame.module.css';
 
 const RecentGame = ({ game }: { game: Game[] }) => {
   return (
     <>
       {
         game?.map((g) => (
-          <div key={g.id}>
-            <div>{g.gameMode === GameModeEnum.Rank ? '랭크' : '일반'}</div>
-            {/* TODO: show gameInfo pretty */}
-            {JSON.stringify(g.gameInfo)}
+          <div key={g.id} className={styles.game}>
+            <div className={styles.gameMode}>게임모드 : {g.gameMode === GameModeEnum.Rank ? '랭크' : '일반'}</div>
+            <div className={styles.createdAt}>게임시간 : {g.createdAt}</div>
+            <div className={styles.nickname}>게임참여자 : {JSON.stringify(g.gameInfo.nickname)}</div>
+            <div className={styles.score}>게임점수 : {JSON.stringify(g.gameInfo.score)}</div>
           </div>        
         ))
       }
--- frontend/src/type/profileType.ts
+++ frontend/src/type/profileType.ts
@@ -1,7 +1,8 @@
 export type Game = {
     id: number;
+    createdAt: string;
     gameMode: number;
-    gameInfo: JSON;
+    gameInfo: any;
 };
 
 export enum GameModeEnum {
